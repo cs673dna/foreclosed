bc8019fd51051ba58a9c0e572f11af775e7040fe
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-12 18:56:29 -0500

    adding zillow ZWSID

--- views.py
+++ views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from HAMP import HAMPForm
 
 
 def hello(request):
 	return HttpResponse("Hello World")
 
 def index(request):
-	t = loader.get_template('index.html')
+	t = loader.get_template('index.html', {'form': form})
 	c = Context()
 
-	return HttpResponse (t.render(c))
+	return HttpResponse(t.render(c))
+
+def HAMP(request):
+	t = loader.get_template('HAMP_FORM.html')
+	c = Context()
+
+	return HttpResponse(t.render(c))
--- zillow.py
+++ zillow.py
@@ -1,2 +1,4 @@
+ZWSID = 'X1-ZWz1byi0snpg5n_7x5fi'
+
 def zEstimate(address):
 	return 120000.00
