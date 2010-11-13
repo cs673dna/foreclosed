cb0f83e021388972aa40dadac85b3219820b7aa9
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-13 14:20:00 -0500

    removed things that didn't work

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.template import Context, loader
-from HAMP import HAMPForm
+#from HAMP import HAMPForm
 
 
 def hello(request):
 	return HttpResponse("Hello World")
 
 def index(request):
-	t = loader.get_template('index.html', {'form': form})
+	t = loader.get_template('index.html')
 	c = Context()
 
 	return HttpResponse(t.render(c))
@@ -16,4 +16,10 @@ def HAMP(request):
 	t = loader.get_template('HAMP_FORM.html')
 	c = Context()
 
+	return HttpResponse(t.render(c))
+
+def HAMPResults(request):
+	t = loader.get_template('HAMP_RESULT.html')
+	c = Context()
+	
 	return HttpResponse(t.render(c))
