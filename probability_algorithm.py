54c6580ff33f8abe4a52c74e06ae63c797fed742
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-13 14:26:57 -0500

    couple of syntax fixes

--- probability_algorithm.py
+++ probability_algorithm.py
@@ -1,25 +1,25 @@
 from decimal import *
-class ProbabilityAlgo():
+
+class ProbabilityAlgorithm():
 	def __init__(self):
 		pass
 
-	def hasNegativeEquity(assessedValue, amountOwed)
-		if amountOwed > assessedValue
+	def hasNegativeEquity(assessedValue, amountOwed):
+		if amountOwed > assessedValue:
 			return True
 		else:	
 			return False
 	
-	def probabilityOfForeclosure (assessedValue, amountOwed)
-		if hasNegativeEquity(assessedValue, amountOwed) = False
+	def probabilityOfForeclosure (assessedValue, amountOwed):
+		if hasNegativeEquity(assessedValue, amountOwed) is False:
 			return "there is no risk of foreclosure if you just sell your house"
 		
 		negativeEquity = Decimal(amountOwed) - Decimal(assessedValue)
 		percentOfNegativeEquity = negativeEquity / assessedValue
 		return	percentValueOfForeclosure(percentOfNegativeEquity)
 
-	def percentValueOfForeclosure(percentOfNegEq)
-		if percentofNegEq > 1.0
+	def percentValueOfForeclosure(percentOfNegEq):
+		if percentofNegEq > 1.0:
 			return 100
-		else
+		else:
 			return 100.0 * percentOfNegEq
-
