3f0c54df2e4607f4a93c156c06ff93dd06f9ee96
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-12 20:08:27 -0500

    hello world zillow

--- zillow.py
+++ zillow.py
@@ -1,4 +1,14 @@
+from string import Template
+import urllib2
+import urllib
+
 ZWSID = 'X1-ZWz1byi0snpg5n_7x5fi'
+ZID = 'cs673dna'
+
+GetSearchResultsTemplate = Template('http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=$ZWSID&address=$address&citystatezip=$citystatezip')
 
 def zEstimate(address):
-	return 120000.00
+	uri=GetSearchResultsTemplate.substitute(ZWSID=ZWSID, address=urllib.quote(address.address), citystatezip=urllib.quote(address.citystatezip))
+	print uri
+	f = urllib2.urlopen(uri)
+	return f
