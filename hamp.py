d8f7007b1bba854adc78542d61fd561569485b55
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-20 12:55:39 -0500

    trying to get HAMP form to work

--- hamp.py
+++ hamp.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class HAMPForm(forms.Form):
-	numberOfUnits = forms.CharField()
-	freddiFannie = forms.RadioSelect()
+	numberOfUnits = forms.CharField(label="Number of Units", help_text="Number of units in the property whose mortage you would like to modify")
+	freddiFannie = forms.BooleanField()
