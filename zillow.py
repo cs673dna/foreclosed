6cdaeea35eaa32d44ffa3e7a0df58f77376ee02d
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-28 11:41:20 -0500

    added exception class, add checking for error message from zillow

--- zillow.py
+++ zillow.py
@@ -11,12 +11,22 @@ GetSearchResultsTemplate = Template('http://www.zillow.com/webservice/GetSearchR
 def zEstimate(address):
 	uri=GetSearchResultsTemplate.substitute(ZWSID=ZWSID, address=urllib.quote(address.address), citystatezip=urllib.quote(address.citystatezip))
 	f = urllib2.urlopen(uri)
-	return f
+	return parseSearchResults(f)
 
 
 def parseSearchResults(results):
 	resultsXML = parse(results)	
-	responseXML = resultsXML.firstChild.getElementsByTagName('response')[0]
+	try:
+		responseXML = resultsXML.firstChild.getElementsByTagName('response')[0]
+	except IndexError as e:
+		raise ZillowError(resultsXML.getElementsByTagName("message")[0].getElementsByTagName("text")[0].firstChild.toxml())
 	zestimateXML = responseXML.getElementsByTagName('zestimate')[0]
 	amountXML = zestimateXML.getElementsByTagName('amount')[0]
 	return amountXML.firstChild.toxml()
+
+
+class ZillowError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
