327175ee30f94070d770f8208156edac72802164
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-28 11:33:52 -0500

    fixing various problems with algorithm

--- hamp_algorithm.py
+++ hamp_algorithm.py
@@ -7,12 +7,14 @@ MAX_UNITS = 5
 
 def meetsModificationRequirements(monthlyIncome, mortage, address):
 
-	if not monthlyIncome * PAYMENT_TO_INCOME_GUIDELINE > mortage.monthlyPayment:
+	if monthlyIncome * PAYMENT_TO_INCOME_GUIDELINE > mortage.monthlyPayment:
 		return False, "Monthly Income too high relative to mortage payment for HAMP modification."
 
-	if not (mortage.OwnerOccupied and mortage.units < MAX_UNITS):
-		return False, "Mortage must be on an owner occupied \
-			house with less than %s unis for HAMP modification." % (MAX_UNITS)
+	if not mortage.ownerOccupied:
+		return False, "Mortgage must be on an owner-occupied house"
+
+	if mortage.units > MAX_UNITS:
+		return False, "Mortage must be on a house with less than %s units for HAMP modification." % (MAX_UNITS)
 
 	if not mortage.first:
 		return False, "Only first mortages can be modified with HAMP."
@@ -20,14 +22,14 @@ def meetsModificationRequirements(monthlyIncome, mortage, address):
 	return mortageMeetsHAMPRequirements(mortage, address) 
 
 
-def mortageMeetsHAMPRequirements(mortage, address):
-	if not mortage.amountowed > address.AssessedValue:
+def mortgageMeetsHAMPRequirements(mortage, address):
+	if mortage.amountOwed > address.assessedValue():
 		return False, "Amount owed must be less than assessed value for HAMP modifiaction."
 
-	if not mortage.amountowed <= MAX_MODIFICATION_AMOUNT:
+	if mortage.amountOwed > MAX_MODIFICATION_AMOUNT:
 		return False, "Amount owed must be less than %s" % (MAX_MODIFICATION_AMOUNT)
 
 	if not mortage.current:
 		return False, "You must be current on your mortage to recieve HAMP modification."
 
-	return True
+	return True,
