ee426d60830728198b953892138bfc3a2e754a5e
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-28 17:02:41 -0500

    renaming functions

--- foreclosure_probability.py
+++ foreclosure_probability.py
@@ -1,24 +1,23 @@
-class ProbabilityAlgorithm():
+
+def foreclosureProbability (request):
+	if not hasNegativeEquity(assessedValue, amountOwed): 
+		return 0, "You do not have negative equity, \
+			there is no probability of forecosure\
+			if you sell your house."
 	
-	def probabilityOfForeclosure (request):
-		if not hasNegativeEquity(assessedValue, amountOwed): 
-			return 0, "You do not have negative equity, \
-				there is no probability of forecosure\
-				if you sell your house."
-		
-		negativeEquity = amountOwed - assessedValue
-		percentOfNegativeEquity = negativeEquity / assessedValue
-		return	percentValueOfForeclosure(percentOfNegativeEquity)
+	negativeEquity = amountOwed - assessedValue
+	percentOfNegativeEquity = negativeEquity / assessedValue
+	return	percentValueOfForeclosure(percentOfNegativeEquity)
 
-	def percentValueOfForeclosure(percentOfNegEq):
-		if percentofNegEq > 1:
-			return 1
-		else:
-			return 1 * percentOfNegEq
+def percentValueOfForeclosure(percentOfNegEq):
+	if percentofNegEq > 1:
+		return 1
+	else:
+		return 1 * percentOfNegEq
 
 
-	def hasNegativeEquity(assessedValue, amountOwed):
-		if amountOwed > assessedValue:
-			return True
-		else:	
-			return False
+def hasNegativeEquity(assessedValue, amountOwed):
+	if amountOwed > assessedValue:
+		return True
+	else:	
+		return False
