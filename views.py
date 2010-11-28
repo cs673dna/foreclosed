259fdbdc46338335160a70560729c4293c54abe8
andrew_kittredge <andrewlkittredge@gmail.com>
2010-11-28 16:43:19 -0500

    removed references to HAMP

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
-from hamp import HAMPForm, HAMP
 from django.shortcuts import render_to_response
 
 def index(request):
