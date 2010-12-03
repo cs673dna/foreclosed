cc3f239e2f0c983fa83fcdffa3b5d7e034f01da8
andrew_kittredge <andrewlkittredge@gmail.com>
2010-12-02 20:44:30 -0500

    major rework, the user input form renders

--- foreclosure_probability.py
+++ foreclosure_probability.py
@@ -1,35 +1,43 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django import forms
 
-def foreclosureProbability (request):
+class ForeclosureProbabilityForm(forms.Form):
+
+	amount_owed = forms.DecimalField(label="Amount Owed")
+	steet_address = forms.CharField(label="Address")
+	city_state_zip = forms.CharField(label="City, State, Zip")	
+	
+
+def foreclosure_probability(request):
 	if request.method == 'POST':
-		form = ForeclosureProbability(request.POST)	
+		form = ForeclosureProbabilityForm(request.POST)	
 		if form.is_valid():
-			return processForm(form)
-
+			return _process_form(form)
 	else:
-		form = ForeclosureProbability()
+		form = ForeclosureProbabilityForm()
 		
-	return render_to_response('foreclosure_probability_entry.html', 
+	return render_to_response(
+		'foreclosure_probability_entry.html', 
 		{'form': form}, 
 		context_instance = RequestContext(request))
 
 
-def processForm(form):
+def _process_form(request):
 	mortgageAddress = Address(form.cleaned_data['street_address'],
 		form.cleaned_data['city_state_zip'])
 
-	amountOwed = form.cleaned_data['amount_owed']
+	amount_owed = form.cleaned_data['amount_owed']
 
-	return_values = foreclosureProbability(mortgageAddress.assessedValue(), 
-		amountOwed)
+	return_values = calc_foreclosure_probability(mortgageAddress.assessedValue(), 
+		amount_owed)
 	
 	return render_to_response('foreclose_probability_result.html',
 		{'probability': return_values[0], 
 			'algorithm_message': return_values[1]})
 	
 
-def foreclosureProbability(assessedValue, amountOwed):
+def calc_foreclosure_probability(assessedValue, amountOwed):
 	
 	if not hasNegativeEquity(assessedValue, amountOwed): 
 		return 0, "You do not have negative equity, \
@@ -52,7 +60,3 @@ def hasNegativeEquity(assessedValue, amountOwed):
 		return True
 	else:	
 		return False
-
-class ForeclosureProbability(forms.form):
-	def __init__():
-		pass	
