10b9a4cd3e2b32ad3e5c93fcc4e00c47ca8dce7e
andrew_kittredge <andrewlkittredge@gmail.com>
2010-12-06 21:36:04 -0500

    minor edits

--- user_mortgage_data.py
+++ user_mortgage_data.py
@@ -1,6 +1,7 @@
 from django.db import models
+from address.address import Address
 
 class AmountOwedFromUser(models.Model):
-	amount_owed = models.FloatField(decimal_places=2)
+	amount_owed = models.FloatField()
 	address = models.ForeignKey(Address)
 	date_time_collected = models.DateTimeField()
