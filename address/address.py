1b1c0c2c076ee0f12dacf8b8c15bf17f7b50d94c
andrew_kittredge <andrewlkittredge@gmail.com>
2010-12-06 21:27:21 -0500

    making some changes

--- address/address.py
+++ address/address.py
@@ -1,10 +1,12 @@
 from zillow import zEstimate
+from django.db import models
 
-class Address():
-	def __init__(self, address, citystatezip):
-		self.address = address
-		self.citystatezip = citystatezip
-
+class Address(models.Model):
+	street_address = models.CharField(max_length=100)
+	city_state_zip = models.CharField("City, State, Zip", max_length=200)
+	#def __init__(self, address, citystatezip):
+		#self.street_adress = address
+		#self.cits_state_zip = citystatezip
 
 	def assessedValue(self):
-		return zEstimate(self.address, self.citystatezip)
+		return zEstimate(self.street_address, self.city_state_zip)
