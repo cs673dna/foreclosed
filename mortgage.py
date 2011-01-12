b7e5d003eda47205c4bf4cc19e5ab438d980861d
andrew_kittredge <andrewlkittredge@gmail.com>
2011-01-11 21:29:09 -0500

    Wasn't using the mortgage class

--- mortgage.py
+++
@@ -1,8 +0,0 @@
-class Mortgage():
-	def __init__(self, monthlyPayment, amountOwed, first, ownerOccupied, units, current):
-		self.monthlyPayment = monthlyPayment
-		self.amountOwed = amountOwed
-		self.first = first
-		self.ownerOccupied = ownerOccupied
-		self.units = units
-		self.current = current
